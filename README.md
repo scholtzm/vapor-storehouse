a75f02fc5f1c8e084ae47a6c2538ac1890e64164
Michael Scholtz <michael.scholtz@outlook.com>
2015-06-14 18:45:38 +0200

    Added very simple offer handling for received offers.

--- README.md
+++ README.md
@@ -4,16 +4,25 @@
 
 ### Features
 
-TODO
+WIP
 
 ### Methods
 
-TODO
+WIP
 
 ### Installation
 
 1. Go to your Vapor folder.
 2. Run `npm install git+https://github.com/scholtzm/vapor-tradeoffers.git`.
+3. Open your config file and update `pluginOptions` to include settings for this plugin. It should look something like this:
+
+```json
+"pluginOptions": {
+  "vapor-tradeoffers": {
+    "familyViewPIN": "1234"
+  }
+}
+```
 
 ### License
 
--- index.js
+++ index.js
@@ -1,1 +1,101 @@
+var appinfo = require('./package.json');
+
+var fs = require('fs');
 var TradeOfferManager = require('steam-tradeoffer-manager');
+
+module.exports = function(VaporAPI) {
+
+    var utils = VaporAPI.utils;
+    var log = VaporAPI.getLogger(appinfo.name);
+    var client = VaporAPI.getClient();
+    var config = VaporAPI.getConfig(appinfo.name);
+    var POLLDATA_PATH = VaporAPI.getPluginFolderPath(appinfo.name) + '/polldata.json';
+
+    var manager = new TradeOfferManager(client, null, 'en');
+
+    // Restore poll data if possible.
+    if(fs.existsSync(POLLDATA_PATH)) {
+        manager.pollData = JSON.parse(fs.readFileSync(POLLDATA_PATH));
+    }
+
+    // Register handler for event when we receive our cookies.
+    VaporAPI.registerHandler('vapor', 'cookies', function(cookies) {
+        manager.setCookies(cookies, function(error) {
+            if(error) {
+                log.error(err);
+                return;
+            }
+
+            log.info('Received API key.');
+
+            // We will also unlock family view if necessary.
+            if(config && config.familyViewPIN) {
+                manager.parentalUnlock(config.familyViewPIN, function(error) {
+                    if(error) {
+                        log.error('Error with parental unlock: ' + error);
+                        return;
+                    }
+
+                    log.info('Family View has been unlocked.');
+                });
+            }
+        });
+    });
+
+    /**
+     * Register different trade offer manager handlers.
+     */
+     // Data polling handler.
+    manager.on('pollData', function(pollData) {
+        log.debug('Received new poll data.');
+        fs.writeFile(POLLDATA_PATH, JSON.stringify(pollData));
+    });
+
+
+    manager.on('newOffer', function(offer) {
+        var sid = offer.partner.getSteamID64();
+        var username = client.users[sid] !== undefined ? ' (' + client.users[sid].playerName + ')' : '';
+
+        log.info('New offer #' + offer.id + ' from ' + sid + username);
+
+        if(utils.isAdmin(sid)) {
+            offer.accept(function(error) {
+                if(error)
+                    log.warn('Trade offer was not accepted. Retrying ...');
+                else
+                    log.info('Trade offer was accepted successfully.');
+            });
+        } else {
+            offer.decline(function(error) {
+                if(error)
+                    log.warn('Trade offer was not declined.');
+                else
+                    log.info('Trade offer was declined successfully.');
+            });
+        }
+    });
+
+
+    manager.on('receivedOfferChanged', function(offer, oldState) {
+        log.info('Offer #' + offer.id + ' changed status from ' + TradeOfferManager.getStateName(oldState) + ' to ' + TradeOfferManager.getStateName(offer.state));
+
+        if(offer.state == TradeOfferManager.ETradeOfferState.Accepted) {
+            offer.getReceivedItems(function(error, items) {
+                if(error) {
+                    log.warn('Couldn\'t get received items: ' + error);
+                } else {
+                    var names = items.map(function(item) {
+                        return item.name;
+                    });
+
+                    log.info('Received items: ' + names.join(', '));
+                }
+            });
+        }
+    });
+
+
+    // Export manager into API.
+    VaporAPI.TradeOfferManager = manager;
+
+};
